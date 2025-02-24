32b69369a142d692f950e29e73f67fdfce6632ff
Bander Al-Mutairi <73838482+B6xb@users.noreply.github.com>
2025-02-24 23:52:07 +0300

    Initial Commit

---
+++ Clipper/ClipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipperApp.swift
+//  Clipper
+//
+//  Created by Bander Almutairi on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ClipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
